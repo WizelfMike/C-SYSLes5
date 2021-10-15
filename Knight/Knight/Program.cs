82451b9ea01910f9d7e3d0e9127cefd7be13fa06
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2021-10-15 11:26:43 +0200

    Did Lesson 5, hopefully with everything included. If not, go ahead and tell me what i could have done differently

---
+++ Knight/Knight/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Knight
+{
+    abstract class Equipment
+    {
+        public int x;
+        public int y;
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public virtual void Equip()
+        {
+            Console.WriteLine("Equipment slot");
+        }
+
+        public virtual void Hitbymouse(int x, int y)
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+
+    abstract class Consumable
+    {
+        public virtual void Consume()
+        {
+            Console.WriteLine("Gulp");
+        }
+
+        public virtual void Spend()
+        {
+            Console.WriteLine("Money, Money, Mone- AAAAAAAAAH AAAAAAAAAAAAH AAAAAAAAAAAAAAAAAAAAAAAAH");
+        }
+
+        public bool CanConsume = true;
+    }
+
+    class Armor : Equipment
+    {
+        public override void Equip()
+        {
+            Console.WriteLine("Armor slot");
+        }
+
+        public override void Hitbymouse(int x, int y)
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+
+    class Weapon : Equipment
+    {
+        public override void Equip()
+        {
+            Console.WriteLine("Weapon slot");
+        }
+
+        public override void Hitbymouse(int x, int y)
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+
+    class Money : Consumable
+    {
+        public bool CanPickup = true;
+    }
+
+    class PotionHeal : Consumable
+    {
+        public override void Consume()
+        {
+            Console.WriteLine("Yummy Health");
+        }
+    }
+
+    class PotionMana : Consumable
+    {
+        public override void Consume()
+        {
+            Console.WriteLine("Yummy Mana");
+        }
+    }
+
+    
+}
